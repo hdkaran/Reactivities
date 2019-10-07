8fb851582b8aa02ffbe7cb7be33043f7080ca53f
hdkaran <karanbhatia0161@gmail.com>
2019-10-07 20:04:38 +1100

    Added Crud Functionality

---
+++ Application/Activities/List.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using MediatR;
+using System;
+using Persistence;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>>
+        {
+
+        }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext dataContext) {
+                _context=dataContext;
+            }
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await _context.Activities.ToListAsync();
+
+                return activities;
+            }
+        }
+    }
+}
